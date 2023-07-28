f4e64ef0ce2e674c504f14c6e55de6ef85d82617
veemola <violetachieng848@gmail.com>
2023-07-28 12:39:58 -0700

    Copy-cut-paste

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+word = "Holberton"
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
+print(word[0:2])
+print(word[7:9])
+print(word[4])
