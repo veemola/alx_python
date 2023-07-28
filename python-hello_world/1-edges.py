3729df64fd8d7661bd30a74d504ba271d0190a50
veemola <violetachieng848@gmail.com>
2023-07-28 12:52:00 -0700

    Copy-cut-paste

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 word = "Holberton"
+word_first_3 = "Hol"
 print("First 3 letters: {}".format(word_first_3))
+word_last_2 = "on"
 print("Last 2 letters: {}".format(word_last_2))
+middle_word = "olberto"
 print("Middle word: {}".format(middle_word))
-print(word[0:2])
-print(word[7:9])
-print(word[4])
